6b08b908b89c984aa0028391e3172ea500aaa9d2
Jackson Carpenter <carpenterjack20@gmail.com>
2023-10-03 16:02:12 -0600

    Learning in-line comments and variable declaration

---
+++ JavaLearning.js
@@ -0,0 +1,17 @@
+// This is an in-line comment.
+
+/* This is a 
+multi-line comment */
+
+// Creating variables for my age, first, and last name and assigning values
+
+var myAge = 32;
+var myFirstName = "Papa";
+var myLastName = "Potato";
+
+// Creating a variable and then assigning it the value of myAge
+
+var wifeAge;
+wifeAge = myAge - 3;
+wifeFirstName = "Mama";
+wifeLastName = myLastName;
