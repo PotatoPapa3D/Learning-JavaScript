ff6afae548fd74736e5a5076b1946cdfa8c8eb5f
Jackson Carpenter <carpenterjack20@gmail.com>
2023-10-23 16:30:21 -0600

    added more and modified a few variables

--- JavaLearning.js
+++ JavaLearning.js
@@ -5,9 +5,9 @@ multi-line comment */
 
 // Creating variables for my age, first, and last name and assigning values
 
-var myAge = 32;
+let myAge = 32;
 var myFirstName = "Papa";
-var myLastName = "Potato";
+const myLastName = "Potato";
 
 // Creating a variable and then assigning it the value of myAge
 
@@ -15,3 +15,15 @@ var wifeAge;
 wifeAge = myAge - 3;
 wifeFirstName = "Mama";
 wifeLastName = myLastName;
+
+/// Using Math Operators
+addition = 4 + 5;
+subtraction = addition - 5;
+multiplication = subtraction * 5;
+division = multiplication / 5;
+/// Add 1
+division++;
+/// Subtract 1
+division--;
+/// Creating a remainder variable
+remainder =  19 / 4
