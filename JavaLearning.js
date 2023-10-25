b71e8c7ae585ae92796c856c65134c21711d906d
Jackson Carpenter <carpenterjack20@gmail.com>
2023-10-25 16:10:50 -0600

    More JS learnings

--- JavaLearning.js
+++ JavaLearning.js
@@ -8,6 +8,7 @@ multi-line comment */
 let myAge = 32;
 var myFirstName = "Papa";
 const myLastName = "Potato";
+myfullname = myFirstName + " " + myLastName;
 
 // Creating a variable and then assigning it the value of myAge
 
@@ -15,6 +16,8 @@ var wifeAge;
 wifeAge = myAge - 3;
 wifeFirstName = "Mama";
 wifeLastName = myLastName;
+wifeFullName = wifeFirstName + " " + wifeLastName;
+countWifeFullName = wifeFullName.length;
 
 /// Using Math Operators
 addition = 4 + 5;
@@ -26,4 +29,21 @@ division++;
 /// Subtract 1
 division--;
 /// Creating a remainder variable
-remainder =  19 / 4
+remainder =  19 / 4;
+/// Creating arrays
+myFirstArray = ["BBQ", 365];
+nestedArray = [["Stockton", 12], ["Fox", 5]];
+const multiDimensionalArr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+  ];
+const subarray = multiDimensionalArr[3]; /// assigns [[10, 11, 12], 13, 14]
+const nestedSubarray = multiDimensionalArr[3][0]; /// assigns  [10, 11, 12]
+const element = multiDimensionalArr[3][0][1]; /// assigns 11
+nestedArray.push(['Curry', 30]); /// adds ['Curry', 30]
+nestedArray.pop(); /// drops ['Curry', 30]
+nestedArray.shift(); /// drops ["Stockton", 12]
+nestedArray.unshift(["Stockton", 12]); /// adds ["Stockton", 12] to beginning
+
